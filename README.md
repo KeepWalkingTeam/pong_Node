d8fb157d886d66b8918efe4509587cf3ae6ba15a
cezar teste <cezar teste>
2016-11-28 21:05:22 -0200

    Fist commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pong - Keep Walking 
+
+This is a experiment. A game for the Hackathon from VanHackathon Web Site.
+
+Basically, it's a pong game using a web page. Page:(Principal.html) and  a Smarthphone control (Celular2.html)
+
+Enjoy!! 
---
+++ index.js
@@ -0,0 +1,76 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+app.use(express.static(__dirname + '/public'));
+var numUsers = 0;
+
+io.on('connection', function (socket) 
+{
+  var addedUser = false;
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message',
+	{
+      username: socket.username,
+      message: data
+    });
+  });
+ 
+  // when the client emits 'add user', this listens and executes
+  socket.on('user', function (username)
+  {  
+    if ((addedUser) || (numUsers === 2)) return;
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  socket.on('up', function (data)
+  {
+    socket.broadcast.emit('up',      
+	{
+      username: socket.username,
+      message: data
+    });  
+  });
+
+  socket.on('down', function (data)
+  {
+    socket.broadcast.emit('down',      
+	{
+      username: socket.username,
+      message: data
+    });  
+  });
+  // when the user disconnects.. perform this
+  socket.on('disconnect',function() 
+  {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
